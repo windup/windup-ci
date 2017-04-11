72d51bc096fe7eec645ca140595420cdff68e559
Jess Sightler <jesse.sightler@gmail.com>
2017-04-11 12:04:59 -0400

    Switched to using the windup-web-distribution Dockerfile

--- windup-nightly-public-docker/build.sh
+++ windup-nightly-public-docker/build.sh
@@ -8,67 +8,77 @@ BUILD_DATE=`date +%Y%m%d`
 
 cd $DIR
 
+rm -rf windup-web
+rm -rf windup-keycloak-tool
+rm -rf windup-web-distribution
+
+git clone https://github.com/windup/windup-web.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web) failed"
+        exit 1
+fi
+
+git clone https://github.com/windup/windup-keycloak-tool.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web-keycloak-tool) failed"
+        exit 1
+fi
+
+
+git clone https://github.com/windup/windup-web-distribution.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web-distribution) failed"
+        exit 1
+fi
 
 cd windup-web
-git checkout -- .
-git clean -f -d
-git pull
+mvn clean install -DskipTests
 if [ $? != 0 ]; then
-	echo "Git pull failed"
-	exit 1
-fi
-mvn clean
-if [ $? != 0 ]; then
-	echo "Maven clean failed"
-	exit 1
-fi
-mvn install -DskipTests
-if [ $?	!= 0 ];	then
-        echo "Maven build failed"
+        echo "Maven build failed for windup-web-keycloak-tool"
         exit 1
 fi
 cd ..
-rm -rf image/keycloak*
-rm -rf image/META-INF
-rm -rf image/wildfly*
-unzip -d image/ windup-web/tests/wildfly-dist/target/windup-web-tests-wildfly-dist-*-SNAPSHOT.jar
-if [ $?	!= 0 ];	then
-        echo "Wildfly unzip failed"
+
+
+cd windup-keycloak-tool
+mvn clean install
+if [ $? != 0 ]; then
+        echo "Maven build failed for windup-web-keycloak-tool"
+        exit 1
+fi
+cd ..
+
+
+cd windup-web-distribution
+mvn clean install
+if [ $? != 0 ]; then
+        echo "Maven build failed for windup-web-distribution"
         exit 1
 fi
 
-mv image/wildfly-*.Final image/wildfly
+unzip -d target/ target/rhamt-web-distribution-*.zip
 if [ $?	!= 0 ];	then
-        echo "Wildfly copy failed"
+        echo "Distribution unzip failed"
         exit 1
 fi
 
-./image/wildfly/bin/jboss-cli.sh --commands='embed-server -c=standalone-full.xml,/subsystem=datasources/data-source=WindupServicesDS:add(jndi-name="java:jboss/datasources/WindupServicesDS", connection-url="jdbc:h2:${jboss.server.data.dir}/h2/windup-web", driver-name="h2", max-pool-size=30, user-name=sa, password=sa)'
-./image/wildfly/bin/jboss-cli.sh --commands='embed-server -c=standalone-full.xml,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=max-post-size, value=524288000)'
-./image/wildfly/bin/jboss-cli.sh --commands='embed-server -c=standalone-full.xml,/system-property=keycloak.server.url:write-attribute(name=value, value=${env.KEYCLOAK_URL:/auth})'
-
-# Copy services
-cp -R windup-web/services/target/rhamt-web/api image/wildfly/standalone/deployments/api.war
+rm target/rhamt-web-distribution-*.zip
+mv target/rhamt-web-distribution-* target/rhamt-web-distribution
 if [ $?	!= 0 ];	then
-        echo "Windup Web Services Copy failed"
+        echo "RHAMT copy failed"
         exit 1
 fi
-touch image/wildfly/standalone/deployments/api.war.dodeploy
 
-# Copy UI
-cp -R windup-web/ui/target/rhamt-web image/wildfly/standalone/deployments/rhamt-web.war
-if [ $?	!= 0 ];	then
-        echo "Windup Web UI Copy failed"
-        exit 1
-fi
-touch image/wildfly/standalone/deployments/rhamt-web.war.dodeploy
 
-cd image
+cd target/rhamt-web-distribution
+./switch_to_authentication_required.sh
+
 docker build -t=docker.io/windup3/windup-web_nightly .
 if [ $?	!= 0 ];	then
         echo "Docker build failed"
         exit 1
 fi
+
 docker push docker.io/windup3/windup-web_nightly
 if [ $?	!= 0 ];	then
         echo "Docker push failed"
