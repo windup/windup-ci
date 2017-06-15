fd9d633e9cbfea7bb5e5aeb59a9a02c33c022ef6
David Klingenberg <dklingen@redhat.com>
2017-06-15 15:44:33 +0200

    Windup web dockerization

---
+++ windup-web-docker-compose/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+MAVEN_HOME=/var/lib/jenkins/tools/apache-maven-3.3.9
+export PATH=$PATH:$MAVEN_HOME/bin
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DATE=`date +%Y%m%d`
+
+cd $DIR
+
+rm -rf windup-web
+rm -rf windup-keycloak-tool
+rm -rf windup-web-distribution
+
+git clone https://github.com/windup/windup-web.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web) failed"
+        exit 1
+fi
+
+git clone https://github.com/windup/windup-keycloak-tool.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web-keycloak-tool) failed"
+        exit 1
+fi
+
+
+git clone https://github.com/windup/windup-web-distribution.git
+if [ $? != 0 ]; then
+        echo "Git clone (windup-web-distribution) failed"
+        exit 1
+fi
+
+cd windup-web
+mvn clean install -DskipTests -Dwebpack.environment=production
+if [ $? != 0 ]; then
+        echo "Maven build failed for windup-web"
+        exit 1
+fi
+cd ..
+
+
+cd windup-keycloak-tool
+mvn clean install
+if [ $? != 0 ]; then
+        echo "Maven build failed for windup-web-keycloak-tool"
+        exit 1
+fi
+cd ..
+
+
+cd windup-web-distribution
+mvn clean install
+if [ $? != 0 ]; then
+        echo "Maven build failed for windup-web-distribution"
+        exit 1
+fi
+
+unzip -d target/ target/rhamt-web-distribution-*.zip
+if [ $?	!= 0 ];	then
+        echo "Distribution unzip failed"
+        exit 1
+fi
+
+rm target/rhamt-web-distribution-*.zip
+mv target/rhamt-web-distribution-* target/rhamt-web-distribution
+if [ $?	!= 0 ];	then
+        echo "RHAMT copy failed"
+        exit 1
+fi
+
+
+cd target/rhamt-web-distribution
